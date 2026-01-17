6e72ee87d07c019b773691c4973c57c2da7e3a11
l1ght-man <dahlamarzouki@gmail.com>
2026-01-17 14:01:45 +0100

    getway detection

--- network_monitor.py
+++ network_monitor.py
@@ -2,6 +2,24 @@ import subprocess
 import platform
 import time
 import datetime
+def get_getway():
+    result= subprocess.run(['ipconfig'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,text=True)
+    output= result.stdout.lower()
+    lines= output.split('\n')
+    for line in lines:
+        if 'default gateway' in line and '.' in line and "1" in line:
+            parts = line.split(':')
+            if len(parts) > 1 :
+                gateway = parts[1].strip().split()[0]
+                if gateway.startswith('192.') or gateway.startswith('10.') or gateway.startstwith('172.') : return gateway
+    return None 
+
+
+gateway= get_getway()
+if gateway :
+    hosts= ['8.8.8.8',gateway,'google.com','1.1.1.1','127.0.0.1']
+else: 
+    hosts= ['8.8.8.8','google.com','1.1.1.1','127.0.0.1']
 
 def ping_host(host):
     parm= '-n' if platform.system().lower() == 'windows' else '-c'
@@ -9,7 +27,6 @@ def ping_host(host):
     result= subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     return result.returncode == 0
 
-hosts= ['8.8.8.8','google.com','1.1.1.1']
 while True:
     now = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
     for host in hosts:
